85f9822a9210e7aed601afc0300c154681dff867
Clemens Kofler <clemens@railway.at>
2014-04-19 18:01:02 +0200

    Start drafting up a custom packages buildpack

---
+++ lib/custom_packages.rb
@@ -0,0 +1,42 @@
+require 'yaml'
+
+require 'pathname'
+require 'fileutils'
+require 'tmpdir'
+
+module CustomPackages
+  class PackageInstaller
+    include ::CustomPackages::ShellHelpers
+
+    def initialize(*args)
+      @build_dir, @cache_dir = args
+
+      @target_dir = Pathname.new(File.join(@build_dir, 'tmp', 'packages'))
+
+      prepare
+    end
+
+    def prepare
+      FileUtils.rm_rf(@target_dir)
+      FileUtils.mkdir_p(@target_dir)
+      Dir.chdir(@target_dir)
+    end
+
+    def install_packages(package_file)
+      @config, @packages = YAML.load_file(package_file).values_at('config', 'packages')
+
+      @packages.each do |package, steps|
+        topic "Installing #{package}"
+
+        steps.each do |step|
+          puts step
+          run(step)
+        end
+      end
+    end
+
+    def cleanup
+      FileUtils.rm_rf(@target_dir)
+    end
+  end
+end
